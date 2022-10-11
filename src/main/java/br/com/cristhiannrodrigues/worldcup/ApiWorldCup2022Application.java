015329330502bec5aede9c2d9530dd239d63402c
Cristhiann Rodrigues <cristhiann.martins@opensingular.com>
2022-10-11 16:26:23 -0300

    Starting Project

---
+++ src/main/java/br/com/cristhiannrodrigues/worldcup/ApiWorldCup2022Application.java
@@ -0,0 +1,13 @@
+package br.com.cristhiannrodrigues.worldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorldCup2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWorldCup2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cristhiannrodrigues/worldcup/ApiWorldCup2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cristhiannrodrigues.worldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWorldCup2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
